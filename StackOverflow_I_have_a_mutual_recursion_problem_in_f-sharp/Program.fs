be25b25e4f0757b8fd025279cb040e6364c8714f
Richard Freytag <rfreytag@freytag.us>
2020-12-17 16:20:25 -0500

    Solution to the problem given here: https://stackoverflow.com/questions/65307389/i-have-a-mutual-recursion-problem-in-f-sharp

--- StackOverflow_I_have_a_mutual_recursion_problem_in_f-sharp/Program.fs
+++ StackOverflow_I_have_a_mutual_recursion_problem_in_f-sharp/Program.fs
@@ -4,33 +4,43 @@ open System
 
 // I have the following types in F#:
 
-type Name = string;;
+type Name = string
 type Sex = 
     | M // male
     | F // female
 type YearOfBirth = int;;
-type FamilyTree = P of Name * Sex * YearOfBirth * Children
-and Children = FamilyTree list;;
+type FamilyTree = 
+    | Person of Name * Sex * YearOfBirth * Children
+    | Nothing 
+and 
+    Children = FamilyTree list
 
 //here is an example:
-let f1 = P("Larry",M,1920,[P("May",F,1945,[P("Fred",M,1970,[])]);P("Joe",M,1950,[P("Adam",M,1970,[])]);P("Paul",M,1955,[])])
+let family_example = [Person("Larry",M,1920,
+                        [Person("May",F,1945,
+                            [Person("Fred",M,1970,[])]);
+                             Person("Joe",M,1950,
+                                [Person("Adam",M,1970,[])]);
+                             Person("Paul",M,1955,[])
+                             ]
+                         )]
 
 (*My task is to create a function: find: Name -> FamilyTree -> returns (found name, sex, year, [List of the names of all their children]
 
 I know it has something to do with mutual recursion but I am not sure how to apply it. This is what I wrote so far:*)
 
-let fstn (f:FamilyTree) =
+let firstName (f:FamilyTree) =
     match f with
-    |P(n,s,y,c) -> n
+    |Person( name, _, _, _) ->  name
 
-let rec find n t = function
-    |P(n1,s1,y1,cs) -> if n1=n then (n1,s1,y1,List.map (fun x -> fstn x) cs) 
-                           else findC n cs
-and findC n clist =
-    match clist with
-    |[] -> []
-    |c::cs -> if n = fstn c then find n c 
-                            else findC n cs
+let rec find name ( familyTree_list : FamilyTree list) : FamilyTree =
+    match familyTree_list with
+    | (person::_) when name = (firstName person) -> person 
+    | (Person( _, _, _, children)::siblings) when (children <> [] || siblings <> []) ->
+        let depth_search = find name children
+        if Nothing <> depth_search then depth_search
+        else find name siblings
+    | _ -> Nothing
 
 (*When I run
 
@@ -48,5 +58,7 @@ Does anyone know what I am doing wrong? I know there is a problem with types but
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "%A" (find "May" family_example)
+    printfn "%A" (find "Joe" family_example)
+    printfn "%A" (find "no one" family_example)
     0 // return an integer exit code
